50c9967a3aa37b93808d8b7f24aea38bc8f4bc02
Douglas M <douglasmassolari@hotmail.com>
2022-03-24 10:41:43 -0300

    feat: adicionar mapeamento para comandos do coc

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -89,6 +89,7 @@ wk.register({
   c = {
     name = 'Code',
     a = { "<cmd>CocAction<CR>", 'Ações' },
+    c = { "<cmd>Telescope coc commands<CR>", 'Comandos'},
     d = { '<cmd>Telescope coc workspace_diagnostics<cr>', 'Problemas (diagnostics)' },
     -- d = { '<cmd>CocList diagnostics<cr>', 'Problemas (diagnostics)' },
     f = { "<cmd>call CocActionAsync('format')<CR>", 'Formatar código' },
