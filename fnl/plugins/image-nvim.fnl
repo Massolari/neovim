3dbb2987a4913bf5123a862460a85e33805b4895
Douglas M. <douglasmassolari@hotmail.com>
2024-08-14 11:51:16 +0200

    feat(plugins): adicionar image.nvim

---
+++ fnl/plugins/image-nvim.fnl
@@ -0,0 +1,4 @@
+[
+ {1 :vhyrro/luarocks.nvim :priority 1001 :opts {:rocks [:magick]}}
+ {1 :3rd/image.nvim :config true}
+ ]
--- init.lua
+++ init.lua
@@ -41,3 +41,7 @@ require("tangerine").setup({
   },
   keymaps = {},
 })
+
+-- Example for configuring Neovim to load user-installed installed Lua rocks:
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua"
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua"
