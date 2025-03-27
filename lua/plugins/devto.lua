b8a95748a8efd58e6fd6ef635a3492c5844533ff
Douglas M. <douglasmassolari@hotmail.com>
2025-03-27 16:57:02 +0100

    refactor(plugin): não usa plugins locais

--- lua/plugins/devto.lua
+++ lua/plugins/devto.lua
@@ -1,11 +1,11 @@
-local functions = require("functions")
+-- local functions = require("functions")
 
-local plugin_dir = (vim.g.local_plugins_dir .. "/devto.nvim")
+-- local plugin_dir = (vim.g.local_plugins_dir .. "/devto.nvim")
 
 return {
   "Massolari/devto.nvim",
-  cond = functions.dir_exists(plugin_dir),
+  -- cond = functions.dir_exists(plugin_dir),
+  -- dir = plugin_dir,
   config = true,
-  dir = plugin_dir,
   cmd = "Devto",
 }
--- lua/plugins/lsp-auto-setup.lua
+++ lua/plugins/lsp-auto-setup.lua
@@ -1,11 +1,11 @@
 local functions = require("functions")
 
-local plugin_dir = vim.g.local_plugins_dir .. "/lsp-auto-setup.nvim"
+-- local plugin_dir = vim.g.local_plugins_dir .. "/lsp-auto-setup.nvim"
 
 return {
   "Massolari/lsp-auto-setup.nvim",
-  cond = functions.dir_exists(plugin_dir),
-  dir = plugin_dir,
+  -- cond = functions.dir_exists(plugin_dir),
+  -- dir = plugin_dir,
   event = "BufRead",
   dependencies = { "neovim/nvim-lspconfig" },
   opts = { server_config = functions.lsp_config_options },
