cb6cb16c703fd695165e5bc4f6c901387c67cc79
Douglas M. <douglasmassolari@hotmail.com>
2025-04-22 17:45:02 +0200

    refactor: passar configuração diretamente para `lsp-auto-setup`

--- lua/plugins/lsp-auto-setup.lua
+++ lua/plugins/lsp-auto-setup.lua
@@ -9,5 +9,61 @@ return {
   event = "BufRead",
   config = true,
   dependencies = { "neovim/nvim-lspconfig" },
-  opts = { server_config = functions.lsp_config_options },
+  opts = {
+    server_config = {
+      eslint = function()
+        return {
+          root_dir = function(fname)
+            return require("lspconfig").eslint.config_def.default_config.root_dir(fname)
+          end,
+        }
+      end,
+      ltex_plus = function(default_config)
+        return {
+          root_dir = vim.uv.cwd,
+          filetypes = { "octo", unpack(default_config.filetypes) },
+          settings = {
+            ltex = {
+              enabled = {
+                "bibtex",
+                "context",
+                "context.tex",
+                "gitcommit",
+                "html",
+                "latex",
+                "markdown",
+                "octo",
+                "org",
+                "restructuredtext",
+                "rsweave",
+              },
+            },
+          },
+        }
+      end,
+      tailwindcss = function(default_config)
+        return {
+          settings = {
+            tailwindCSS = {
+              includeLanguages = { elm = "html", gleam = "html" },
+              experimental = {
+                classRegex = {
+                  '\\bclass[\\s(<|]+"([^"]*)"',
+                  '\\bclass[\\s(]+"[^"]*"[\\s+]+"([^"]*)"',
+                  '\\bclass[\\s<|]+"[^"]*"\\s*\\+{2}\\s*" ([^"]*)"',
+                  '\\bclass[\\s<|]+"[^"]*"\\s*\\+{2}\\s*" [^"]*"\\s*\\+{2}\\s*" ([^"]*)"',
+                  '\\bclass[\\s<|]+"[^"]*"\\s*\\+{2}\\s*" [^"]*"\\s*\\+{2}\\s*" [^"]*"\\s*\\+{2}\\s*" ([^"]*)"',
+                  '\\bclassList[\\s\\[\\(]+"([^"]*)"',
+                  '\\bclassList[\\s\\[\\(]+"[^"]*",\\s[^\\)]+\\)[\\s\\[\\(,]+"([^"]*)"',
+                  '\\bclassList[\\s\\[\\(]+"[^"]*",\\s[^\\)]+\\)[\\s\\[\\(,]+"[^"]*",\\s[^\\)]+\\)[\\s\\[\\(,]+"([^"]*)"',
+                },
+              },
+            },
+          },
+          init_options = { userLanguages = { elm = "html", gleam = "html" } },
+          filetypes = { "elm", "gleam", unpack(default_config.filetypes) },
+        }
+      end,
+    },
+  },
 }
