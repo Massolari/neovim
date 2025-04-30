ff425cbbcb9702244197ebdf79570008fe2f889c
Douglas M. <douglasmassolari@hotmail.com>
2025-04-30 09:18:08 +0200

    feat(lua_ls): adiciona todo o runtime no library

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -2,10 +2,14 @@ return {
   on_init = function(client)
     client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
       runtime = { version = "LuaJIT" },
-      diagnostics = { unusedLocalIgnore = { "_*" } },
+      diagnostics = { unusedLocalIgnore = { "_*" }, globals = { "Snacks" } },
       hint = { enable = true },
-      workspace = { library = { vim.env.VIMRUNTIME } },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
     })
   end,
-  settings = { Lua = {} },
+  settings = {
+    Lua = {},
+  },
 }
