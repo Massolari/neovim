da4e9379d548eaba62bf4e08a91f33d5d841129a
Douglas M. <douglasmassolari@hotmail.com>
2025-06-03 09:04:47 +0200

    feat(treesitter): define `branch` para `master`

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   event = "BufReadPost",
+  branch = "master",
   dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
   build = ":TSUpdate",
   opts = {
