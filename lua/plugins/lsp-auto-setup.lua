b33c89ad3213e002451f459db866a9f848e5c8a4
Douglas M. <douglasmassolari@hotmail.com>
2025-09-18 10:38:34 +0200

    feat(lsp-auto-setup): adiciona configuração para ts_ls

--- lua/plugins/lsp-auto-setup.lua
+++ lua/plugins/lsp-auto-setup.lua
@@ -12,6 +12,7 @@ return {
   opts = {
     server_config = {
       ltex_plus = function(default_config)
+        --- @type vim.lsp.Config
         return {
           root_dir = vim.uv.cwd,
           filetypes = { "octo", unpack(default_config.filetypes) },
@@ -35,6 +36,7 @@ return {
         }
       end,
       tailwindcss = function(default_config)
+        --- @type vim.lsp.Config
         return {
           settings = {
             tailwindCSS = {
@@ -55,6 +57,38 @@ return {
           },
           init_options = { userLanguages = { elm = "html", gleam = "html" } },
           filetypes = { "elm", "gleam", unpack(default_config.filetypes) },
+        }
+      end,
+      ts_ls = function(default_config)
+        --- @type vim.lsp.Config
+        return {
+          on_attach = function(client, bufnr)
+            local cmd_description = "Go to source definition"
+
+            vim.keymap.set("n", "gri", function()
+              local position_params = vim.lsp.util.make_position_params(0, client.offset_encoding)
+
+              client:exec_cmd({
+                title = cmd_description,
+                command = "_typescript.goToSourceDefinition",
+                arguments = { vim.uri_from_bufnr(0), position_params.position },
+              }, { bufnr = 0 }, function(error, result)
+                if error then
+                  vim.notify("Error when executing command: " .. error.message, vim.log.levels.ERROR)
+                  return
+                end
+                if result and #result > 0 then
+                  local items = vim.lsp.util.locations_to_items(result, client.offset_encoding)
+                  vim.fn.setqflist(items)
+                  vim.lsp.util.show_document(result[1], client.offset_encoding)
+                end
+              end)
+            end, { desc = cmd_description })
+
+            if default_config.on_attach then
+              default_config.on_attach(client, bufnr)
+            end
+          end,
         }
       end,
     },
