4eb49ab9948332145041b6906bd7645be30e3520
Douglas M. <douglasmassolari@hotmail.com>
2025-10-22 17:54:35 +0200

    chore(plugins): remove leetcode

--- lua/plugins/leetcode.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "kawre/leetcode.nvim",
-  build = ":TSUpdate html",
-  cmd = "Leet",
-  dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
-  opts = { lang = "elixir" },
-}
-
