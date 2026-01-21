4702e6be074554561cfb55abc3f8971dd52f14a9
Douglas M. <douglasmassolari@hotmail.com>
2026-01-21 10:52:55 +0100

    refactor(sidekick): migrar keymaps de lazy para config
    - Move todas as keymaps de lazy keys para função config
    - Adiciona chamada require("sidekick").setup()
    - Mantém todas as funcionalidades e modos das keymaps originais
    - Simplifica estrutura usando vim.keymap.set diretamente
    
    Esta mudança organiza melhor as configurações do plugin, centralizando
    a inicialização e keymaps na função config ao invés de usar a
    propriedade keys do lazy.nvim.

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -1,64 +1,41 @@
 --- @type LazyPluginSpec
 return {
   "folke/sidekick.nvim",
-  keys = {
-    {
-      "<tab>",
-      function()
-        -- if there is a next edit, jump to it, otherwise apply it if any
-        if require("sidekick").nes_jump_or_apply() then
-          return
-        end
+  config = function()
+    require("sidekick").setup()
 
-        if vim.lsp.inline_completion.get() then
-          return
-        end
+    vim.keymap.set({ "n", "i" }, "<tab>", function()
+      -- if there is a next edit, jump to it, otherwise apply it if any
+      if require("sidekick").nes_jump_or_apply() then
+        return
+      end
 
-        return "<Tab>" -- fallback to normal tab
-      end,
-      expr = true,
-      desc = "Ir/Aplicar NES (Next Edit Suggestion)",
-      mode = { "n", "i" },
-    },
-    {
-      "<c-q>",
-      function()
-        -- if there is a next edit, jump to it, otherwise apply it if any
-        require("sidekick.nes").update()
-      end,
-      desc = "Atualizar NES",
-    },
-    {
-      "<c-.>",
-      function()
-        require("sidekick.cli").toggle()
-      end,
-      mode = { "n", "x", "i", "t" },
-      desc = "Alterar foco com Sidekick",
-    },
-    {
-      "<leader>iss",
-      function()
-        require("sidekick.cli").toggle({ focus = true })
-      end,
-      desc = "Alternar CLI",
-      mode = { "n", "v" },
-    },
-    {
-      "<leader>isp",
-      function()
-        require("sidekick.cli").select_prompt()
-      end,
-      desc = "Perguntar",
-      mode = { "n", "v" },
-    },
-    {
-      "<leader>isf",
-      function()
-        require("sidekick.cli").send({ msg = "{file}" })
-      end,
-      desc = "Enviar arquivo",
-      mode = { "n", "v" },
-    },
-  },
+      if vim.lsp.inline_completion.get() then
+        return
+      end
+
+      return "<Tab>" -- fallback to normal tab
+    end, { expr = true, desc = "Ir/Aplicar NES (Next Edit Suggestion)" })
+
+    vim.keymap.set("n", "<c-q>", function()
+      -- if there is a next edit, jump to it, otherwise apply it if any
+      require("sidekick.nes").update()
+    end, { desc = "Atualizar NES" })
+
+    vim.keymap.set({ "n", "x", "i", "t" }, "<c-.>", function()
+      require("sidekick.cli").toggle()
+    end, { desc = "Alterar foco com Sidekick" })
+
+    vim.keymap.set({ "n", "v" }, "<leader>iss", function()
+      require("sidekick.cli").toggle({ focus = true })
+    end, { desc = "Alternar CLI" })
+
+    vim.keymap.set({ "n", "v" }, "<leader>isp", function()
+      require("sidekick.cli").select_prompt()
+    end, { desc = "Perguntar" })
+
+    vim.keymap.set({ "n", "v" }, "<leader>isf", function()
+      require("sidekick.cli").send({ msg = "{file}" })
+    end, { desc = "Enviar arquivo" })
+  end,
 }
