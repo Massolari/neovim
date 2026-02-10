769e2ed900fc03aef9236af65aea2695e8003d44
Douglas M. <douglasmassolari@hotmail.com>
2026-02-10 13:30:43 +0100

    feat(neoment): configura notificações

--- lua/plugins/neoment.lua
+++ lua/plugins/neoment.lua
@@ -1,9 +1,28 @@
+local functions = require("functions")
+
+local plugin_dir = vim.g.local_plugins_dir .. "/neoment.nvim"
+
 return {
   "Massolari/neoment.nvim",
+  dir = functions.dir_exists(plugin_dir) and plugin_dir or nil,
   dependencies = { "nvim-lua/plenary.nvim" },
-  init = function()
+  config = function()
     vim.g.neoment = {
-      save_session = true,
+      notifier = function(message, level, opts)
+        local message_without_title = message:sub(#"[Neoment] " + 1)
+        return Snacks.notify(
+          message_without_title,
+          vim.tbl_extend("force", opts or {}, {
+            title = "Neoment",
+            level = level,
+          })
+        )
+      end,
+      desktop_notifications = {
+        per_room = {
+          ["#telegram_vimbr:massolari.us.to"] = "all",
+        },
+      },
     }
   end,
 }
