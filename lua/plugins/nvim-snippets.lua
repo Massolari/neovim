ba9f69f667568855fd901df586a41b057e6a9ebc
Douglas M. <douglasmassolari@hotmail.com>
2026-03-30 11:53:39 +0200

    feat(nvim-snippets): troca para meu repositório

--- lua/plugins/nvim-snippets.lua
+++ lua/plugins/nvim-snippets.lua
@@ -3,19 +3,17 @@ local plugin_dir = vim.g.local_plugins_dir .. "/nvim-snippets"
 
 --- @type LazyPluginSpec
 return {
-  "garymjr/nvim-snippets",
+  "Massolari/nvim-snippets",
   lazy = false,
-  dependencies = { "rafamadriz/friendly-snippets" },
-  opts = {
-    friendly_snippets = true,
-    create_cmp_source = false,
-    create_native_completion = vim.fn.has("nvim-0.12") == 1,
-  },
+  dependencies = { "rafamadriz/friendly-snippets", "onsails/lspkind.nvim" },
   dir = functions.dir_exists(plugin_dir) and plugin_dir or nil,
-  config = function(_, opts)
-    require("snippets").setup(opts)
-    if vim.fn.has("nvim-0.12") == 1 then
-      vim.opt.complete:append({ "Fv:lua.nvim_snippets_complete" })
-    end
+  config = function()
+    require("snippets").setup({
+      friendly_snippets = true,
+      create_cmp_source = false,
+      create_native_completion = true,
+      native_completion_kind = require("lspkind").presets.codicons["Snippet"],
+    })
+    vim.opt.complete:append({ "Fv:lua.nvim_snippets_complete" })
   end,
 }
